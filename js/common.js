1ae25721f2e16089cea8e11289809d82b91e1428
Виктория Захарова <zaharova181114@gmail.com>
2023-07-17 15:58:54 +0300

    Initial commit

---
+++ js/common.js
@@ -0,0 +1,37 @@
+$('[name="phone"]').mask('+7 999 - 999 - 99 - 99');
+
+// sliders
+$('.photo-gallery').slick({
+	slidesToShow: 4,
+	variableWidth: true,
+	centerMode: true,
+	infinite: true,
+	prevArrow: '<button type="button" class="slick-prev"><svg class="svg-icon"><use xlink:href="img/sprite.svg#prev"></use></svg></button>',
+	nextArrow: '<button type="button" class="slick-next"><svg class="svg-icon"><use xlink:href="img/sprite.svg#next"></use></svg></button>'
+});
+
+$('.services-slider').slick({
+	slidesToShow: 3,
+	dots: true,
+	prevArrow: '<button type="button" class="slick-prev"><svg class="svg-icon"><use xlink:href="img/sprite.svg#prev"></use></svg></button>',
+	nextArrow: '<button type="button" class="slick-next"><svg class="svg-icon"><use xlink:href="img/sprite.svg#next"></use></svg></button>'
+});
+
+$('.services-shop-slider').slick({
+	slidesToShow: 4,
+	dots: true,
+	prevArrow: '<button type="button" class="slick-prev"><svg class="svg-icon"><use xlink:href="img/sprite.svg#prev"></use></svg></button>',
+	nextArrow: '<button type="button" class="slick-next"><svg class="svg-icon"><use xlink:href="img/sprite.svg#next"></use></svg></button>'
+});
+
+$('.reviews-slider').slick({
+	slidesToShow: 4,
+	dots: true,
+	prevArrow: '<button type="button" class="slick-prev"><svg class="svg-icon"><use xlink:href="img/sprite.svg#prev"></use></svg></button>',
+	nextArrow: '<button type="button" class="slick-next"><svg class="svg-icon"><use xlink:href="img/sprite.svg#next"></use></svg></button>'
+});
+
+// gallery
+Fancybox.bind("[data-fancybox]", {
+	// Your custom options
+});
