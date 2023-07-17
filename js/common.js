53e65eab4728c1cb861ecff1bc1ee5c9879d0d62
Виктория Захарова <zaharova181114@gmail.com>
2023-07-17 22:32:58 +0300

    media1

--- js/common.js
+++ js/common.js
@@ -21,7 +21,28 @@ $('.services-shop-slider').slick({
 	slidesToShow: 4,
 	dots: true,
 	prevArrow: '<button type="button" class="slick-prev"><svg class="svg-icon"><use xlink:href="img/sprite.svg#prev"></use></svg></button>',
-	nextArrow: '<button type="button" class="slick-next"><svg class="svg-icon"><use xlink:href="img/sprite.svg#next"></use></svg></button>'
+	nextArrow: '<button type="button" class="slick-next"><svg class="svg-icon"><use xlink:href="img/sprite.svg#next"></use></svg></button>',
+	responsive: [
+		{
+			breakpoint: 1200,
+			settings: {
+				slidesToShow: 3,
+			}
+		},
+		{
+			breakpoint: 768,
+			settings: {
+				slidesToShow: 2,
+			}
+		},
+		{
+			breakpoint: 576,
+			settings: {
+				slidesToShow: 1,
+				arrows: false
+			}
+		}
+	]
 });
 
 $('.reviews-slider').slick({
